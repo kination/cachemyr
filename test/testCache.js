c8fca99072cedede06851334a91a9b5ce786b632
kwangin.jung <inylove82@gmail.com>
2019-10-28 19:05:54 +0900

    Update package and test

--- test/testCache.js
+++ test/testCache.js
@@ -1,12 +1,11 @@
 const assert = require('assert')
 const cache = require('../dist/index')
 
-describe('Test cache', () => {
+describe('Test module', () => {
   
-  it('Test read/write cache', () => {
+  it('Test read/write', () => {
     cache.put('key', 1)
     assert.equal(cache.get('key'), 1)
-    
   })
 
   it('Test remove', () => {
@@ -16,49 +15,12 @@ describe('Test cache', () => {
     assert.equal(cache.get('key'), '')
   })
 
-  it ('Test timeout', (done) => {
+  it ('Test timeout expiration', (done) => {
     cache.put('key-timeout', 'value-timeout', 3000)
     assert.equal(cache.get('key-timeout'), 'value-timeout')
     setTimeout(() => {
       assert.equal(cache.get('key-timeout'), '')
       done()
     }, 4000)
-
   })
-  
-
-  it('Test performance', () => {
-    const hrstart = process.hrtime()
-    console.log('\nTest heap status ====== ')
-    let total = process.memoryUsage().heapTotal / 1024 / 1024
-    console.log('total:' + total)
-    let used = process.memoryUsage().heapUsed / 1024 / 1024
-    console.log('used:' + used)
-
-    for (let a = 0; a < 2; a++) {
-      for (let i = 0; i < 1000000; i++) {
-        cache.put(`key-${i}`, `val-${i}`)
-      }
-
-      console.log('\nAfter fill cache ====== ')
-      console.log('demo cache:' + cache.get('key-1'))
-      total = process.memoryUsage().heapTotal / 1024 / 1024
-      console.log('total:' + total)
-      used = process.memoryUsage().heapUsed / 1024 / 1024
-      console.log('used:' + used)
-
-      cache.drop()
-
-      console.log('\nAfter drop cache ====== ')
-      console.log('demo cache:' + cache.get('key-1'))
-      total = process.memoryUsage().heapTotal / 1024 / 1024
-      console.log('total:' + total)
-      used = process.memoryUsage().heapUsed / 1024 / 1024
-      console.log('used:' + used)
-    }
-
-    const hrend = process.hrtime(hrstart)
-    console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
-  })
-
 })
